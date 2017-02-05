2f893e0a6ae929e551f9cbf2b350e3bdcd1db6f7
J <github@927589452.de>
2017-02-05 23:14:00 +0100

    added some backup scripts

---
+++ backup_solution/generic_cleanup_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Simple backup with rsync
+# local-mode, tossh-mode, fromssh-mode
+
+SOURCES= ( /storage/900B-8135/DCIM/ /storage/emulated/0/DCIM/ /storage/emulated/0/Pictures/)
+TARGET="/home/jens/Pictures/"
+
+# edit or comment with "#"
+#LISTPACKAGES=listdebianpackages        # local-mode and tossh-mode
+MONTHROTATE=monthrotate                 # use DD instead of YYMMDD
+
+RSYNCCONF=(--exclude=DCIM --exclude=Music --exclude=Android --exclude=LOST.DIR --exclude=)
+
+MAILREC="info.priority@high5.alioth.uberspace.de"
+
+SSHUSER="root"
+FROMSSH="canary"
+#TOSSH="tossh-server"
+SSHPORT=22
+
+##
+
+ for SOURCE in "${SOURCES[@]}"
+    do 	rsync -avR --recursive --backup --backup-dir=backup --suffix=$RANDOM --remove-source "$SSHUSER"@"$FROMSSH":"$SOURCE"  "$TARGET" 
+;done
+  
+  
---
+++ backup_solution/generic_cleanup_script_downloads.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Simple backup with rsync
+# local-mode, tossh-mode, fromssh-mode
+
+SOURCES= " /storage/900B-8135/Download:/ /storage/emulated/0/Download/ /storage/900B-8135/Downloads/ /storage/emulated/0/Downloads/ "
+TARGET="/home/jens/Downloads/"
+
+# edit or comment with "#"
+#LISTPACKAGES=listdebianpackages        # local-mode and tossh-mode
+MONTHROTATE=monthrotate                 # use DD instead of YYMMDD
+
+RSYNCCONF=(--exclude=DCIM --exclude=Music --exclude=Android --exclude=LOST.DIR --exclude=)
+
+MAILREC="info.priority@high5.alioth.uberspace.de"
+
+SSHUSER="root"
+FROMSSH="canary"
+#TOSSH="tossh-server"
+SSHPORT=22
+
+##
+
+ for SOURCE in "${SOURCES[@]}"
+    do 	rsync -avR --recursive --backup --backup-dir=backup --suffix=$RANDOM --remove-source --checksum "$SSHUSER"@"$FROMSSH":"$SOURCE"  "$TARGET" 
+    done
+  
+  
